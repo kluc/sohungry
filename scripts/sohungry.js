8ba15402e644aa9805bf08386c7d7d4d5d1908de
Kelson Luc <kelson.luc@gmail.com>
2014-06-04 21:35:30 -0700

    updated project

---
+++ scripts/sohungry.js
@@ -0,0 +1,13 @@
+
+function ingredientSubmit(){
+	var node = document.createElement("li");
+	var textnode = document.createTextNode("Test");
+	node.appendChild(textnode);
+	document.getElementById('inglist').appendChild(node);
+
+	
+}
+
+function instructionSubmit(){
+
+}
