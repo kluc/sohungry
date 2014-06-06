57082ce10bff278580fa2b9fd7854e4987fb7fb1
Kelson Luc <kelson.luc@gmail.com>
2014-06-05 18:50:53 -0700

    added delete buttons

--- scripts/sohungry.js
+++ scripts/sohungry.js
@@ -12,16 +12,34 @@ function ajaxAdd(){
 
 }
 
+function deleteCheckedItems(){
+	$('input:checkbox.delete').each(function(){
+		if(this.checked){
+			$(this).parent().remove();
+		}
+	});
+}
+
 
 function ingredientSubmit(){
 	var ing = document.getElementById('ing');
 	var amt = document.getElementById('amt');
 	var type = document.getElementById('type');
 	//alert(ing + amt + type);
+	
+	
+	
 	var node = document.createElement("li");
 	var textnode = document.createTextNode(amt.value + " " + type.value + " " + ing.value);
-	node.appendChild(textnode);
 	document.getElementById('inglist').appendChild(node);
+	
+	
+	var d = document.createElement("input");
+	d.className = "delete";
+	d.setAttribute("type", "checkbox");
+	//document.getElementById('inglist').appendChild(node);
+	node.appendChild(d);
+	node.appendChild(textnode);
 	ing.value = "";
 	amt.value = "";
 	$('type option').prop('selected', function(){
@@ -33,7 +51,13 @@ function instructionSubmit(){
 	var step = document.getElementById('step');
 	var node = document.createElement("li");
 	var textnode = document.createTextNode(step.value);
+	var d = document.createElement("input");
+	d.className = "delete";
+	d.setAttribute("type", "checkbox");
+	node.appendChild(d);
 	node.appendChild(textnode);
+
 	document.getElementById('instlist').appendChild(node);
+	
 	step.value = "";
 }
